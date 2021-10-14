08c1f0f4f691cb7b7959bb00f07d0faac2b44b8e
Agboola Iyanu <agboolasolomoniyanu@gmail.com>
2021-10-14 08:21:00 +0100

    404

--- src/pages/PageNotFound/PageNotFound.jsx
+++ src/pages/PageNotFound/PageNotFound.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
+import Layout from '../../components/Layout/Layout';
 
 const PageNotFound = () => {
-  return <div>404</div>;
+  return (
+    <Layout>Sorry this page does not exist. How did you ever get here?</Layout>
+  );
 };
 
 export default PageNotFound;
