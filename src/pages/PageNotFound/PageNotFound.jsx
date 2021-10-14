05a5deb026c5dfbbb69cce906d07de91efd7fad2
Agboola Iyanu <agboolasolomoniyanu@gmail.com>
2021-10-14 08:42:21 +0100

    added gh-pages

--- src/pages/PageNotFound/PageNotFound.jsx
+++ src/pages/PageNotFound/PageNotFound.jsx
@@ -3,9 +3,7 @@ import Layout from '../../components/Layout/Layout';
 
 const PageNotFound = () => {
   return (
-    <Layout>
-      Ooops! Sorry, this page does not exist. How did you ever get here?
-    </Layout>
+    <Layout>Sorry this page does not exist. How did you ever get here?</Layout>
   );
 };
 
